255319f323228bdca9a0b1da0915bb7278caf5ad
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-01-06 08:58:26 +0530

    started project

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+    // db params
+    define('DB_HOST','localhost');
+    define('DB_USER','__YOUR_USER__');
+    define('DB_PASS','__YOUR_PASSWORD__');
+    define('DB_NAME','__YOUR_DATABASE_NAME__');
+
+    // app root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT','__YOUR_URL__');
+
+    // site name
+    define('SITENAME','__YOUR_SITE_NAME__');
