6a320594a9027e0435b7243bb60423a4e13dd007
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-01-06 10:30:22 +0530

    users controller

---
+++ app/controllers/Users.php
@@ -0,0 +1,30 @@
+<?php
+    class Users extends Controller {
+
+        public function __construct(){
+
+        }
+
+        public function register(){
+            // check for post
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                // process form
+            }else{
+                // init data
+                $data = [
+                  'name' => '',
+                  'email' => '',
+                  'password' => '',
+                    'confirm_password' => '',
+                   'name_error' => '',
+                   'email_error' => '',
+                   'password_error' => '',
+                   'confirm_password_error' => '',
+                ];
+
+                //load view
+                $this->view('users/register',$data);
+            }
+        }
+
+    }
